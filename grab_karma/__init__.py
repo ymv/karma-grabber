d50afbf701e51c867589207fc11f35255b35bb27
Yourkov Mikhail <yourkovm@gmail.com>
2013-06-17 15:21:46 +0400

    Grab voters

--- grab_karma/__init__.py
+++ grab_karma/__init__.py
@@ -39,6 +39,8 @@ def main():
     for user in args.users:
         try:
             data = grabber.grab(user)
+            if dumper.voters:
+                data['voters'] = grabber.grab_voters(data['id'])
         except NotFoundError:
             dumper.not_found(user)
         except AuthError:
@@ -50,12 +52,14 @@ def main():
     dumper.end()
 
 class Dumper(object):
+    voters = False
     def start(self): pass
     def end(self): pass
     def found(self, user, data): pass
     def not_found(self, user): pass
 
 class JSONDumper(Dumper):
+    voters = True
     def start(self):
         self._buffer = {'timestamp': int(time.time()), 'users': {}}
     def end(self):
@@ -66,6 +70,7 @@ class JSONDumper(Dumper):
         self._buffer['users'][user] = None
 
 class TSVDumper(Dumper):
+    voters = False
     def found(self, user, data):
         print '\t'.join(['%d' % time.time(), user, str(data['comment_karma']), str(data['karma'])])
     def not_found(self, user):
@@ -84,12 +89,22 @@ class Grabber(object):
         }
 
     def grab(self, user):
-        data = self._load('/users/'+user)
+        data = self._load('/users/'+user, None)
         tree = lxml.html.document_fromstring(data)
         return self._parse(tree)
 
-    def _load(self, url):
-        self._con.request('GET', url, headers=self._headers)
+    def grab_voters(self, user_id):
+        data = self._load('/karmactl', 'view=%s' % user_id)
+        obj = json.loads(data)
+        return {x['login']: int(x['attitude']) for x in obj['votes']}
+
+    def _load(self, url, data):
+        if data is None:
+            self._con.request('GET', url, headers=self._headers)
+        else:
+            headers = dict(self._headers)
+            headers['Content-Type'] = 'application/x-www-form-urlencoded'
+            self._con.request('POST', url, body=data, headers=headers)
         response = self._con.getresponse()
         data = response.read()
         if response.status == 404:
@@ -103,6 +118,7 @@ class Grabber(object):
     def _parse(self, tree):
         rating_match = re.search(u'([-\d]+).+?([-\d]+).+?([-\d]+)', tree.cssselect('.userrating')[0].text_content())
         return {
+            'id': int(tree.cssselect('#uservote .vote')[0].get('uid')),
             'karma': int(tree.cssselect('.uservoteholder span em')[0].text),
             'comment_karma': int(rating_match.group(3)),
             'post_count': int(rating_match.group(1)),
